88cd8af95b24d29e4db831f1040c02ee389e70a2
mkendall07 <emailmatthere@gmail.com>
2017-03-24 15:03:26 -0400

    -test

--- src/adloader.js
+++ src/adloader.js
@@ -112,5 +112,5 @@ exports.trackPixel = function(pixelUrl) {
     'rnd=' +
     Math.floor(Math.random() * 1e7);
   new Image().src = trackingPixel;
-  return trackingPixel;
+  return    trackingPixel;
 };
